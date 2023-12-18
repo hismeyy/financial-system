9c0a65b06263aa923c96b1916d8afd0226d1d4fd
MaxCosmos <renyh2001@outlook.com>
2023-12-18 16:20:43 +0800

    引入layui

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css'
 
-createApp(App).mount('#app')
+createApp(App).use(Layui).mount('#app')
